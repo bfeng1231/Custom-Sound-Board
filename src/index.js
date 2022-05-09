1cf9137414ebbc8fe8d3acbd194ff99fd2837b47
bfeng1231 <borisfeng1231@gmail.com>
2022-05-09 15:59:42 -0400

    updated redux

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
-import {createStore, applyMiddleware} from 'redux';
+import { applyMiddleware} from 'redux';
+import { configureStore } from '@reduxjs/toolkit'
 import {Provider} from 'react-redux';
 import rootReducer from './reducers/rootReducer';
 import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+//const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = configureStore({
+    reducer: rootReducer
+});
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
